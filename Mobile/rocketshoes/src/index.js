5772e581604002076fd8a37d144b31fcf8bb4bc4
matheuslenke <matheus.l.coutinho@aluno.ufes.br>
2020-03-05 21:12:47 -0300

    fix: moving files

---
+++ Mobile/rocketshoes/src/index.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+import './config/ReactotronConfig';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+      </View>
+    </>
+  );
+}
